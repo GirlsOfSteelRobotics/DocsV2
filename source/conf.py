2a2d136197f42c25c824da02efa587c5c37b060a
PJ Reiniger <pj.reiniger@gmail.com>
2020-04-06 21:10:10 -0400

    Changing default entry point

--- source/conf.py
+++ source/conf.py
@@ -43,6 +43,8 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# Specify the master doc file, AKA our homepage
+master_doc = "index"
 
 # -- Options for HTML output -------------------------------------------------
 
